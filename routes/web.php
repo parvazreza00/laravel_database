e2a1fad92a4c3e9bd9f07a04da904e08fcf98618
parvaz <parvazreza00@gmail.com>
2024-08-29 11:13:45 +0600

    debugger package install

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/', function () {
+
+    $users = User::all();
+    // dd($users);
+    dump($users);
+
+
     return view('welcome');
 });
