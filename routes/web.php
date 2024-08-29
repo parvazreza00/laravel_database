88fb9df7b705d6b7bd2399ca604bf7ff37638c17
parvaz <parvazreza00@gmail.com>
2024-08-29 18:18:08 +0600

    db raw query

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
 
-    $users = User::all();
+    // $users = User::all();
     // dd($users);
-    dump($users);
+    // // dump($users);
+    // $pdo = DB::Connection('mysql')->getPdo();
+    // $users = $pdo->query('select * from users where id=50')->fetchAll();
+    // $users = DB::select('select * from users where id = :id', ['id'=>23]);
+    // $users = DB::select('select * from users');
+    // $users = DB::select('select * from users where id = ?', [1]);
+    // $users = DB::select('select * from users where id = 1');
+    // $users = DB::insert('insert into users (name, email, phone, address, password)
+    // values (?, ?, ?, ?, ?)', ['thamin','thamin@gmail.com','01712345678','dhaka','password']);
+    $users = DB::table('users')->orderBy('id','desc')->first();
 
+
+    // foreach($users as $user){
+    //     echo $user->name;
+    //     echo "<br>";
+    // }
+    dump($users);
 
     return view('welcome');
 });
