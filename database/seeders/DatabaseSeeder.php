1921e2c83df64151c6c6bf9c4a90de6c5278bb4c
parvaz <parvazreza00@gmail.com>
2024-09-02 18:07:35 +0600

    city table uploaded

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             UsersTableSeeder::class,
             CommentSeeder::class,
             RoomSeeder::class,
+            CitySeeder::class,
             ReservationSeeder::class,
             OrderSeeder::class,
         ]);
--- routes/web.php
+++ routes/web.php
@@ -51,14 +51,94 @@ Route::get('/', function () {
     // ->groupBy('rating')
     // ->having('rating','=',5)
     // ->get();
-    $comments = DB::table('comments')
-    ->skip(5)
-    ->take(2)
+
+    // $comments = DB::table('comments')
+    // ->skip(5)
+    // ->take(2)
+    // ->get();
+
+    // $users = DB::table('users')
+    // ->orderBy('id')
+    // ->chunk(50, function($users){
+    //     foreach ($users as $user) {
+    //         // Print user details
+    //         echo 'User ID: ' . $user->id . ', Name: ' . $user->name . '<br>';
+    //     }
+    // });
+
+    // $comments = DB::table('comments')
+    // ->join('users', 'comments.user_id','=','users.id')
+    // ->select('comments.*','users.name','users.email')
+    // ->orderBy('comments.id')
+    // ->chunk(8, function($comments){
+    //     foreach ($comments as $comment) {
+    //         echo 'Comment ID: ' . $comment->id . ', User: ' . $comment->name . ', Email: ' . $comment->email . ', Rating: ' . $comment->rating . '<br>';
+    //     }
+    // });
+
+    // $results = DB::table('comments')->orderBy('id')->chunkById(5, function($comments){
+    //     foreach($comments as $comment){
+    //         DB::table('comments')
+    //         ->where('id',$comment->id)
+    //         ->update(['rating' => null]);
+    //     }
+    // });
+
+    // $results = DB::table('reservations')
+    // ->join('rooms','reservations.room_id', '=', 'rooms.id')
+    // ->join('users', 'reservations.user_id','=','users.id')
+    // ->join('cities', 'reservations.city_id','=','cities.id')
+    // ->where('rooms.id','>',3)
+    // ->where('users.id','>',1)
+    // ->get();
+    // foreach($results as $result){
+    //     echo "User name $result->name -> $result->check_in - $result->check_out (city Name) $result->name</br>";
+    // }
+
+    // $results = DB::table('reservations')
+    // ->join('rooms', function($join){
+    //     $join->on('reservations.room_id', '=', 'rooms.id')
+    //     ->where('rooms.id','>', 3);
+    // })
+    // ->join('users', function($join){
+    //     $join->on('reservations.user_id','=','users.id')
+    //     ->where('users.id','>',1);
+    // })
+    // ->get();
+
+    // $rooms = DB::table('rooms')->where('rooms.id','>',3);
+    // $users = DB::table('users')->where('users.id','>',1);
+    // $results = DB::table('reservations')
+    // ->joinSub($rooms, 'rooms', function($join){
+    //     $join->on('reservations.room_id', '=', 'rooms.id');
+    // })
+    // ->joinSub($users, 'users', function($join){
+    //     $join->on('reservations.user_id','=','users.id');
+    // })
+    // ->get();
+
+    // $results = DB::table('rooms')
+    // ->leftJoin('reservations','reservations.room_id','=','rooms.id')
+    // ->select('rooms.*','reservations.check_in','reservations.check_out')
+    // ->get();
+
+    // $results = DB::table('rooms')
+    // ->rightJoin('reservations','reservations.room_id','=','rooms.id')
+    // ->select('reservations.*','rooms.room_number')
+    // ->get();
+
+    $results = DB::table('rooms')
+    ->leftJoin('reservations','rooms.id', '=', 'reservations.room_id')
+    ->leftJoin('cities','reservations.city_id', '=', 'cities.id')
+    ->selectRaw('room_size,cities.name, count(reservations.id) as reservations_count')
+    ->groupBy('room_size','cities.name')
+    ->orderByRaw('count(reservations.id) DESC')
     ->get();
+    
+    dump($results);
 
 
-    dump($comments);
   
 
-    return view('welcome');
+    return view('home');
 });
